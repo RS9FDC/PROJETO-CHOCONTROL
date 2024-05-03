850934dd08abbaf7682383b194dc7203eac70a9e
RS9FDC <ruan.santos@sptech.school>
2024-05-03 19:13:30 -0300

    Script do banco corrigido

--- Banco de dados/scritatt.sql
+++ Banco de dados/scritatt.sql
@@ -5,20 +5,20 @@ USE chocontrol;
 CREATE TABLE tbEmpresa(
 idEmpresa char(10) primary key,
 nome varchar(50) not null,
-telefone char(14),
-CNPJ char(14) unique not null,
-cep char(9),
-numero varchar(45),
+telefone char(14)not null,
+CNPJ char(22) unique not null,
+email varchar(45) not null,
+cep char(9) not null,
+numero varchar(15) not null,
 complemento varchar(45)
 );
 
 
 CREATE TABLE tbUsuario(
 idUsuario char(12) primary key,
-senhaUsuario varchar(50) not null,
 nomeFuncionario varchar(70) not null,
 email varchar(70) not null,
-DatNasc date not null,
+senha varchar(30) not null,
 fkEmpresa CHAR(10) not null,
 constraint fkUsuarioEmpresa foreign key (fkEmpresa)
 references tbEmpresa (idEmpresa)
@@ -46,8 +46,8 @@ references tbMaquina (idMaquina)
 
 
 CREATE TABLE tbTemperatura(
-	idTemperatura INT PRIMARY KEY AUTO_INCREMENT
-	,temperatura decimal(5,2)
+	idTemperatura INT PRIMARY KEY AUTO_INCREMENT,
+	temperatura decimal(5,2)
 );
     
     
@@ -62,7 +62,7 @@ references tbsensor (idSensor)
 
 
 
-create table Monitoramento (
+create table tbMonitoramento (
 idMonitoramento char(10) primary key,
 dataHoraLeva datetime,
 picoTemperatura decimal(3,1),
@@ -73,33 +73,33 @@ references tbsensor (idSensor)
 
 
 
-INSERT INTO tbEmpresa (idEmpresa, nome, telefone, CNPJ, cep, numero, complemento) VALUES
-('EMP001', 'Empresa A', '1234567890', '12345678901234', '12345-678', '123', 'Bloco A'),
-('EMP002', 'Empresa B', '0987654321', '98765432109876', '98765-432', '456', 'Bloco B');
+INSERT INTO tbEmpresa (idEmpresa, nome, telefone, CNPJ, email, cep, numero, complemento) VALUES
+('EMP001', 'Empresa A', '(11) 5467-9872', '12345678901234','empresaA@example.com', '12345-678', '123', 'Bloco A'),
+('EMP002', 'Empresa B', '(11) 2487-3106', '98765432109876', 'empresaB@example.com' ,'98765-432', '456', 'Bloco B');
 
-INSERT INTO Usuario (idUsuario, senhaUsuario, nomeFuncionario, email, DatNasc, fkEmpresa) VALUES
-('FUNC001', 'senha123', 'João Silva', 'joao.silva@example.com', '1990-05-15', 'EMP001'),
-('FUNC002', 'abc@123', 'Maria Souza', 'maria.souza@example.com', '1985-10-20', 'EMP002');
+INSERT INTO tbUsuario (idUsuario, nomeFuncionario, email, senha, fkEmpresa) VALUES
+('FUNC001', 'João Silva', 'joao.silva@example.com', 'senha123', 'EMP001'),
+('FUNC002', 'Maria Souza', 'maria.souza@example.com', 'senha321', 'EMP002');
 
 
 
 -- Inserções na tabela Maquina
-INSERT INTO Maquina (idMaquina, modeloMaquina, saborChocolate, fkEmpresa) VALUES
+INSERT INTO tbMaquina (idMaquina, modeloMaquina, saborChocolate, fkEmpresa) VALUES
 ('MAQ001', 'Máquina 1', 'Ao leite', 'EMP001'),
 ('MAQ002', 'Máquina 2', 'Amargo', 'EMP002');
 
 -- Inserções na tabela Sensor
-INSERT INTO Sensor (idSensor, modeloSensor, fkMaquina) VALUES
+INSERT INTO tbSensor (idSensor, modeloSensor, fkMaquina) VALUES
 ('SENSOR001', 'Sensor 1', 'MAQ001'),
 ('SENSOR002', 'Sensor 2', 'MAQ002');
 
 
-INSERT INTO intervaloTemperatura (idIntervaloTemperatura, temperaturaMaxima, temperaturaMinima, fkSensor) VALUES
+INSERT INTO tbIntervalo (idIntervaloTemperatura, temperaturaMaxima, temperaturaMinima, fkSensor) VALUES
 (1, 30.0, 20.0, 'SENSOR001'),
 (2, 35.0, 25.0, 'SENSOR002');
 
 -- Inserções na tabela Monitoramento
-INSERT INTO Monitoramento (idMonitoramento, dataHoraLeva, picoTemperatura, fkSensor) VALUES
+INSERT INTO tbMonitoramento (idMonitoramento, dataHoraLeva, picoTemperatura, fkSensor) VALUES
 ('MON001', '2024-04-21 08:00:00', 28.0, 'SENSOR001'),
 ('MON002', '2024-04-21 08:15:00', 30.0, 'SENSOR002');
 
