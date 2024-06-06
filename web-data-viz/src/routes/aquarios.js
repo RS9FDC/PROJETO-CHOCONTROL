1d91c9d1d8e8198e3bfbee09392f298cbc1a93cb
manuelamrodrigues <manuela.rodrigues@sptech.school>
2024-06-05 21:51:26 -0300

    Integracao

--- web-data-viz/src/routes/aquarios.js
+++ web-data-viz/src/routes/aquarios.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 var aquarioController = require("../controllers/aquarioController");
 
-router.get("/:empresaId", function (req, res) {
-  aquarioController.buscarAquariosPorEmpresa(req, res);
-});
+// router.get("/:empresaId", function (req, res) {
+//   aquarioController.buscarAquariosPorEmpresa(req, res);
+// });
 
 router.post("/cadastrar", function (req, res) {
   aquarioController.cadastrar(req, res);
